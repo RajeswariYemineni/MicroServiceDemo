1f3ebe4a530f2bcc991ba43fe3f82ae5cedc757d
Ira <iraupadhyay53@gmail.com>
2022-04-28 12:56:52 +0530

    first commit

---
+++ room-search/src/main/java/com/sapient/room/search/RoomSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.room.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomSearchApplication.class, args);
+	}
+
+}
---
+++ room-search/src/test/java/com/sapient/room/search/RoomSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.room.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
