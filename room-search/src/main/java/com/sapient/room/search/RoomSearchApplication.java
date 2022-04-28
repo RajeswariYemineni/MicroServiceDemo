6deb6020bf10f4a5a7da773d81628f8ac2aeafa7
Ira <iraupadhyay53@gmail.com>
2022-04-28 15:07:55 +0530

    integration with config server

--- room-search/src/main/java/com/sapient/room/search/RoomSearchApplication.java
+++ room-search/src/main/java/com/sapient/room/search/RoomSearchApplication.java
@@ -2,7 +2,8 @@ package com.sapient.room.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class RoomSearchApplication {
 
